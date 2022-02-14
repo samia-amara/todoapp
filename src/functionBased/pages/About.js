1899fc7958160bd2198346b002ef062bdf71eb05
samia <asamia1@hotmail.fr>
2022-02-14 15:38:45 +0100

    mon premier commit

---
+++ src/functionBased/pages/About.js
@@ -0,0 +1,9 @@
+import React from "react"
+import { Outlet } from "react-router-dom"
+
+const About = () => {
+  //let navigation = useNavigate();
+  //console.log(props)
+  return <div>hello from about page</div>
+}
+export default About
--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import About from "./functionBased/pages/About"
+import NotMatch from "./functionBased/pages/NotMatch"
+import SinglePage from "./functionBased/pages/SinglePage"
 //component file
-import TodoContainer from "./components/TodoContainer"
-
+import TodoContainer from "./functionBased/components/TodoContainer"
+import Navbar from "./functionBased/components/Navbar"
 //stylesheet
-import "./App.css"
+import "./functionBased/App.css"
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <BrowserRouter>
+    <Navbar /> 
+    <Routes>
+     <Route path="/" element={<TodoContainer />} />
+     <Route path="/about" element={<About />} /> 
+     <Route path="/about/:slug" element={<SinglePage />} />  
+    <Route path="*" element={<NotMatch />}/>
+    
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
